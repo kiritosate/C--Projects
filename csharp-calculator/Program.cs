4723841ae13a19028a88153c53edbd60ab92d9cf
kiritosate <perverted.issei@gmail.com>
2021-11-16 22:55:27 -0800

    first commit

---
+++ csharp-calculator/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+
+namespace kiritosate
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            while(true)
+            {
+                int number1, number2, result;
+            string operand;
+
+            Console.Write("enter first number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("enter second number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("use ( + , - , * , /)\nenter the operand to use: ");
+            operand = Console.ReadLine();
+            
+            switch(operand)
+            {
+                case "-":
+                    result = number1 - number2;
+                    break;
+                case "*":
+                    result = number1 * number2;
+                    break;
+                case "/":
+                    result = number1 / number2;
+                    break;
+                default:
+                    result = number1 + number2;
+                    break;
+            }
+
+            Console.WriteLine("The Result in Problem {0} {1} {2} = {3}", number1, operand, number2, result);
+            }
+            
+        }
+    }
+}
