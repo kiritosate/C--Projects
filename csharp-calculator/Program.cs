fbfcee7992eb51a2a1365bbc18ee9b0b9f08063f
kiritosate <perverted.issei@gmail.com>
2021-11-19 00:06:06 -0800

    12:05

--- csharp-calculator/Program.cs
+++ csharp-calculator/Program.cs
@@ -21,25 +21,24 @@ namespace kiritosate
                 Console.Write("use ( + , - , * , /)\nenter the operand to use: ");
                 operand = Console.ReadLine();
             
-            switch(operand)
-            {
-                case "-":
-                    result = number1 - number2;
-                    break;
-                case "*":
-                    result = number1 * number2;
-                    break;
-                case "/":
-                    result = number1 / number2;
-                    break;
-                default:
-                    result = number1 + number2;
-                    break;
-            }
+                switch(operand)
+                {
+                    case "-":
+                        result = number1 - number2;
+                        break;
+                    case "*":
+                        result = number1 * number2;
+                        break;
+                    case "/":
+                        result = number1 / number2;
+                        break;
+                    default:
+                        result = number1 + number2;
+                        break;
+                }
 
-            Console.WriteLine("The Result in Problem {0} {1} {2} = {3}", number1, operand, number2, result);
-            }
-            
+                Console.WriteLine("The Result in Problem {0} {1} {2} = {3}", number1, operand, number2, result);
+                }
         }
     }
 }
---
+++ simple-login-system/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace kiritosate
+{
+    class LoginSystem{
+        public static void Main(string[] args){
+            
+            string username, password;
+
+            Console.Write("input your Username: ");
+            username = Console.ReadLine();
+
+            Console.Write("input your Password: ");
+            password = Console.ReadLine();
+
+            if(username == "admin" && password == "adminpassword"){
+                Console.WriteLine("Welcome Back {0}", admin);
+            }else{
+                Console.WriteLine("Wrong username or password provided...");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
