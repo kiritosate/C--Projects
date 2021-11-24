052fe4ded816c6de17cf12722d88c366bd32dc46
kiritosate <perverted.issei@gmail.com>
2021-11-24 13:06:16 -0800

    commit

--- odd-even-while/Program.cs
+++ odd-even-while/Program.cs
@@ -6,33 +6,52 @@ namespace odd_even_while
     {
         static void Main()
         {
-            int odd = 0;
-            int even = 0;
-            int entries = 0;
-            int i = 1;
-            int temp = 0;
-            char again;
+            //initializing variables
 
+            int odd = 0; //total odd number in entries
+            int even = 0; //total even number in entries
+            int entries = 0; // number of entries you want to check
+            int i = 1; //used to compare in entries or loop. 
+            int temp = 0; //this is where the entered number.
+            char again; //char variable to ask for yes/y or no/n to try again.
+
+
+            /*ask the user to enter number of entries, for example 3 entries will result to
+                Number1: 
+                Number2:
+                Number3:
+            */
             Console.Write("Enter Number of Entries: ");
             entries = Convert.ToInt32(Console.ReadLine());
-        
+
+
+            /* this is where the number loop happens. and will ask number from user,
+                will loop until integer i, is equal to entries which the user enteres;
+
+            */
             while(entries>=i){
                 Console.Write("Number{0}: ", i);
                 temp = Convert.ToInt32(Console.ReadLine());
                 
-                if(temp % 2 == 0){
-                    even++;
+                if(temp % 2 == 0){ //checks whether the entered number is even, and if not then its odd
+                    even++; // increase the total even if true
                 }else{
-                    odd++;
+                    odd++;  //increases the total number of odd if false
                 }
-                i++;
+                i++; //increments variable i every loop until it is equals to number of entries
             }
 
+            /*
+                writes in the console the total odds, and evens
+            */
             Console.WriteLine("Total Odd: {0}", odd);
             Console.WriteLine("Total Even: {0}", even);
 
             
-
+            /*
+                a loop that asks the user if they want to try again, choices are yes or no;
+                if not equals to either y or n, then continue the loop
+            */
             while(true){
                 Console.Write("Try Again[Yy/Nn]?___");
                 again = Convert.ToChar(Console.ReadLine());
